4845032b533f31507ed72c896f34f98b98c88e7a
khalia <khaliaparris01@gmail.com>
2021-11-02 14:46:04 +0000

    finshed code challenge

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 //1
-function areBothSame(){
-  
+function areBothSame(num1, num2){
+  return num1 == num2
 }
-
+// console.log(areBothSame(5,5))
 //2
-function areBothEqual(){
-  
+function areBothEqual(num1, num2){
+  return num1 === num2
 }
-
+// console.log(areBothEqual(2, 2))
 //3
-function areAllFourEqual(){
-  
+function areAllFourEqual(a,b,c,d){
+  if (a ===b && b === c && c === d){
+      return true 
+  }else {
+      return false
+  }
 }
-
+// console.log(areAllFourEqual("2", "2", "2", "2"))
 
 //4
-function areAllFourSame(){
-  
+function areAllFourSame(a,b,c,d){
+    if (a ==b && b == c && c == d){
+      return true 
+  }else {
+      return false
+  }
 }
-
+// console.log(areAllFourSame(true, false, 1, "1"))
 //5
-function areAllFourSameOrEqual(){
+function areAllFourSameOrEqual(a,b,c,d){
+  if (a ===b && b ===c && c ===d){
+      return "equal"
+  }else if ( a ==b && b ==c && c ==d){
+      return "same"
+  }else {
+      return null
+  }
   
 }
 
-
+console.log(areAllFourSameOrEqual(true, true, 1, "1"))
